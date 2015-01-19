f479f15a6a61b399f4081a6f53c14e78511fdc33
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2015-01-19 20:32:18 +0100

    front-end for evaluation tabular plotter (incomplete)

--- evaluation.py
+++ evaluation.py
@@ -5,24 +5,21 @@ import experiment_database
 import texttable as tt
 import re
 import time
+import os
+from collections import OrderedDict
 
-if __name__ == '__main__':
-    # print "List of command line options:"
-    command = ''
-    exp_db = ''
-    if len(sys.argv) > 1:
-        exp_db = sys.argv[1]
-    if len(sys.argv) > 2:
-        command = sys.argv[2]
+from sqlite3 import DatabaseError
 
-    if exp_db and command == 'list':
+
+
+def list_experiments(exp_db):
+    try:
         connection = experiment_database.initalize_database(exp_db)
         rows = experiment_database.list_experiments(connection)
-
         tab = tt.Texttable()
         header = ['Id', 'Terminals', 'Nonterminals', 'Rec. Part.', 'Punct.', 'Corpus', 'Date (start)']
         tab.header(header)
-        tab.set_cols_width([4,9,15,10,6,30,20])
+        tab.set_cols_width([4, 9, 15, 10, 6, 30, 20])
         for row in rows:
             t_row = []
             t_row.append(row[0])
@@ -34,8 +31,8 @@ if __name__ == '__main__':
             else:
                 t_row.append(row[1])
 
-            t_row.append(row[2].replace('_',' '))
-            t_row.append(row[3].replace('_',' '))
+            t_row.append(row[2].replace('_', ' '))
+            t_row.append(row[3].replace('_', ' '))
 
             if row[4]:
                 t_row.append('IGNORE')
@@ -43,7 +40,7 @@ if __name__ == '__main__':
                 t_row.append('YES')
 
             # t_row.append(row[5])
-            match = re.search(r'^.*/([^/]+)$',row[5])
+            match = re.search(r'^.*/([^/]+)$', row[5])
             if match:
                 t_row.append(match.group(1))
             else:
@@ -54,7 +51,78 @@ if __name__ == '__main__':
             # print(t_row)
 
             tab.add_row(t_row)
-
         s = tab.draw()
-
         print s
+        experiment_database.finalize_database(connection)
+    except DatabaseError as e:
+        print 'file at \'' + exp_db + '\' is not a valid experiment database:'
+        print e.message
+
+
+def plot_table(exp_db):
+    print sys.argv
+    experiments = []
+    outfile = ''
+    for arg in sys.argv:
+        match = re.search(r'^--experiments=((?:[0-9]|,|-)+)$', arg)
+        if match:
+            string = match.group(1)
+            while string:
+                match = re.search(r'^(\d+)(?:,((?:\d|,|-)+))?$', string)
+                if match:
+                    experiments.append(int(match.group(1)))
+                    string = match.group(2)
+                    continue
+                match = re.search(r'^(\d+)-(\d+)(?:,((?:\d|,|-)+))?$', string)
+                if match:
+                    low = int(match.group(1))
+                    high = int(match.group(2))
+                    if low < high:
+                        experiments += range(low, high + 1)
+                    else:
+                        print "Error: Parse failure at " + string + ": " + str(low) + " is not smaller than " + str(high)
+                        exit(1)
+                    string = match.group(3)
+                    continue
+                print "Error: Parse failure at " +string
+                exit(1)
+
+        match = re.search(r'^--outfile=(.+)$', arg)
+        if match:
+            path = match.group(1)
+            print path
+            if not os.path.isfile(exp_db):
+                try:
+                    open(path, 'w').close()
+                    os.unlink(path)
+                except IOError:
+                    print 'invalid path: \'' + path + '\''
+                    exit(1)
+
+            outfile = path
+
+    # remove duplicates
+    experiments = list(OrderedDict.fromkeys(experiments))
+
+    print "exp: ", experiments
+    print "out: ", outfile
+
+    connection = experiment_database.initalize_database(exp_db)
+    experiment_database.create_latex_table_from_database(connection, experiments)
+    experiment_database.finalize_database(connection)
+
+if __name__ == '__main__':
+    # print "List of command line options:"
+    command = ''
+    exp_db = ''
+    if len(sys.argv) > 1:
+        exp_db = sys.argv[1]
+    if len(sys.argv) > 2:
+        command = sys.argv[2]
+
+    if not os.path.isfile(exp_db):
+        print 'file not found \'' + exp_db + '\''
+    elif command == 'list':
+        list_experiments(exp_db)
+    elif command == 'plot':
+        plot_table(exp_db)
