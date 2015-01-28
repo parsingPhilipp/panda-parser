9c7556e366ee23a0a104f39048ddbc27f0d3cc96
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2015-01-28 18:31:05 +0100

    updated parser tests

--- unittests/parser_test.py
+++ unittests/parser_test.py
@@ -6,16 +6,114 @@ from lcfrs import *
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.grammar_ab_copy = create_grammar()
+        self.grammar_ab_copy = create_copy_grammar()
+        self.grammar_ab_copy_2 = create_copy_grammar_2()
 
     def test_a4(self):
-        parser = Parser(self.grammar_ab_copy, ['a']* 4 )
-        print parser.query_passive_items('S', [0])
+        word = ['a'] * 4
+        parser = Parser(self.grammar_ab_copy, word)
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 2)
+        print
+
+    def test_aabaab(self):
+        word = ['a', 'a', 'b'] * 2
+        parser = Parser(self.grammar_ab_copy, word )
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 2)
+        print
+
+    def test_abba(self):
+        word = ['a', 'b', 'b', 'a']
+        parser = Parser(self.grammar_ab_copy, word )
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 0)
+        print
+
+    def test_a4_2(self):
+        word = ['a'] * 4
+        parser = Parser(self.grammar_ab_copy_2, word )
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 2)
+        print
+
+    def test_aabaab_2(self):
+        word = ['a', 'a', 'b'] * 2
+        parser = Parser(self.grammar_ab_copy_2, word)
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 2)
+        print
+
+    def test_baabbaab_2(self):
+        word = ['b', 'a', 'a', 'b'] * 2
+        parser = Parser(self.grammar_ab_copy_2, word)
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 2)
+        print
+
+    def test_abba_2(self):
+        word = ['a', 'b', 'b', 'a']
+        parser = Parser(self.grammar_ab_copy_2, word )
+        print "Parse", word
+        counter = 0
+        print "Found items:"
+        for passive_item in parser.query_passive_items('S', [0]):
+            if passive_item.range(0) == Range(0, len(word)):
+                print passive_item
+                counter += 1
+        self.assertEqual(counter, 0)
+        print
+
+    def test_remaining_terminal_function(self):
+        x1 = LCFRS_var(1,0)
+        x2 = LCFRS_var(1,1)
+        lhs = LCFRS_lhs('S')
+        lhs.add_arg(['a', x1, 'a', x2])
+        rule = LCFRS_rule(lhs)
+        n = number_of_consumed_terminals(rule, 0, 3)
+        self.assertEqual(n, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
 
-def create_grammar():
+def create_copy_grammar():
     grammar = LCFRS('S')
 
     x1 = LCFRS_var(1,0)
@@ -41,6 +139,47 @@ def create_grammar():
     lhs5 = LCFRS_lhs('A')
     lhs5.add_arg(['b', x1])
     lhs5.add_arg(['b', x2])
+    grammar.add_rule(lhs5, ['A'])
+
+    lhs6 = LCFRS_lhs('A')
+    lhs6.add_arg(['a'])
+    lhs6.add_arg(['a'])
+    grammar.add_rule(lhs6, [])
+
+    lhs7 = LCFRS_lhs('A')
+    lhs7.add_arg(['b'])
+    lhs7.add_arg(['b'])
+    grammar.add_rule(lhs7, [])
+
+    return grammar
+
+
+def create_copy_grammar_2():
+    grammar = LCFRS('S')
+
+    x1 = LCFRS_var(1,0)
+    x2 = LCFRS_var(1,1)
+
+    lhs1 = LCFRS_lhs('S')
+    lhs1.add_arg([x1, x2])
+    grammar.add_rule(lhs1, ['A'])
+
+    lhs2 = LCFRS_lhs('S')
+    lhs2.add_arg([x1, 'a', x2, 'a'])
+    grammar.add_rule(lhs2, ['A'])
+
+    lhs3 = LCFRS_lhs('S')
+    lhs3.add_arg([x1, 'b', x2, 'b'])
+    grammar.add_rule(lhs3, ['A'])
+
+    lhs4 = LCFRS_lhs('A')
+    lhs4.add_arg([x1, 'a'])
+    lhs4.add_arg([x2, 'a'])
+    grammar.add_rule(lhs4, ['A'])
+
+    lhs5 = LCFRS_lhs('A')
+    lhs5.add_arg([x1, 'b'])
+    lhs5.add_arg([x2, 'b'])
     grammar.add_rule(lhs5, ['A'])
 
     lhs6 = LCFRS_lhs('A')
