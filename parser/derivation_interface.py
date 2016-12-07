059cd23b55470af3e34233140e65787d354d2105
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2016-12-07 16:11:45 +0100

    equal method for Abstract derivations

--- parser/derivation_interface.py
+++ parser/derivation_interface.py
@@ -121,6 +121,21 @@ class AbstractDerivation:
                 the_yield.append(elem)
         return the_yield
 
+    def __eq__(self, other):
+        if not isinstance(other, AbstractDerivation):
+            return False
+        return self.__compare_recursive(self.root_id(), other, other.root_id())
+
+    def __compare_recursive(self, id_self, other, id_other):
+        if self.getRule(id_self) != other.getRule(id_other):
+            return False
+        if len(self.child_ids(id_self)) != len(other.child_ids(id_other)):
+            return False
+        for child_self, child_other in zip(self.child_ids(id_self), other.child_ids(id_other)):
+            if not self.__compare_recursive(child_self, other, child_other):
+                return False
+        return True
+
 def derivation_to_hybrid_tree(der, poss, ordered_labels, construct_token, disconnected=None):
     """
     :param der:
