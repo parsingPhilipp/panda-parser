5c45253e10886dcea8d5c14856e2f3fe69c6b8f4
Johann Seltmann <Johann_Michael.Seltmann@mailbox.tu-dresden.de>
2017-03-29 17:10:57 +0200

    started changing file to run all tests

--- playground/play_recursive_partitioning.py
+++ playground/play_recursive_partitioning.py
@@ -18,11 +18,11 @@ train ='../res/negra-dep/negra-lower-punct-train.conll'
 result = 'recursive-partitoning-results.conll'
 start = 'START'
 term_labelling = d_i.the_terminal_labeling_factory().get_strategy('pos')
-recursive_partitioning = d_i.the_recursive_partitioning_factory().getPartitioning('fanout-1')
-primary_labelling = d_l.the_labeling_factory().create_simple_labeling_strategy('child', 'pos+deprel')
+#recursive_partitioning = d_i.the_recursive_partitioning_factory().getPartitioning('fanout-1')
+#primary_labelling = d_l.the_labeling_factory().create_simple_labeling_strategy('child', 'pos+deprel')
 
 # parser_type = parser.parser_factory.GFParser  # slower, can be used for arbitrary fanout
-parser_type = parser.parser_factory.CFGParser  # potentially faster, only for fanout 1
+#parser_type = parser.parser_factory.CFGParser  # potentially faster, only for fanout 1
 tree_parser = LCFRS_sDCP_Parser  # tree parser to count derivations per hybrid tree
 
 
@@ -38,27 +38,133 @@ test_par3 = (set([1,2,3,4,5]), [(set([2]), []), (set([4]), []), (set([1,3,5]), [
 
 def main(ignore_punctuation=False):
 
-
+    
+    
     #for-loops for trying out the various combinations of parameters
     #for strategy of choosing p
+    #for strategy in ['', '-left-to-right', '-random', '-argmax', '-no-new-nont']:
         #strict vs. child labeling
+    #    for labelling1 in ['strict', 'child']:
             #POS/DEPREL
+    #        for labelling2 in ['pos', 'deprel', 'pos+deprel']:
                 #fanout
+    #            for fanout in [1,2,3]:
                     #parser_type
+    #                if fanout == 1:
+    #                    parser_type = parser.parser_factory.CFGParser #parser only for fanout 1
+    #                    trainAndEval(strategy, labelling1, labelling2, fanout, parser_type)
+    parser_type = parser.parser_factory.GFParser
+    trainAndEval('', 'child', 'pos+deprel', 1, parser_type, ignore_punctuation)
 
 
-    trees = parse_conll_corpus(train, False, train_limit)
-    if ignore_punctuation:
-        trees = disconnect_punctuation(trees)
-    (n_trees, grammar) = d_i.induce_grammar(trees, primary_labelling, term_labelling.token_label, recursive_partitioning, start)
+
+
+
+    #trees = parse_conll_corpus(train, False, train_limit)
+    #if ignore_punctuation:
+    #    trees = disconnect_punctuation(trees)
+    #(n_trees, grammar) = d_i.induce_grammar(trees, primary_labelling, term_labelling.token_label, recursive_partitioning, start)
 
     #
     #
     # grammar is the induced hybrid grammar
     #
     #
-    print "#nonts: ", len(grammar.nonts())
-    print "#rules: ", len(grammar.rules())
+    #print "#nonts: ", len(grammar.nonts())
+    #print "#rules: ", len(grammar.rules())
+
+    #total_time = 0.0
+
+    # The following code works for string parsers for evaluating
+    
+    #parser_type.preprocess_grammar(grammar)
+    
+    #trees = parse_conll_corpus(test, False, test_limit)
+    #if ignore_punctuation:
+    #    trees = disconnect_punctuation(trees)
+    
+    #with open(result, 'w') as result_file:
+    #    failures = 0
+    #    for tree in trees:
+    #        time_stamp = time.clock()
+    
+    #        parser = parser_type(grammar, tree_yield(tree.token_yield()))
+    
+    #        time_stamp = time.clock() - time_stamp
+    #        total_time += time_stamp
+    
+    
+    #        cleaned_tokens = copy.deepcopy(tree.full_token_yield())
+    #        for token in cleaned_tokens:
+    #            token.set_deprel('_')
+    #        h_tree = HybridTree(tree.sent_label())
+    #        h_tree = parser.dcp_hybrid_tree_best_derivation(h_tree, cleaned_tokens, ignore_punctuation,
+    #                                                        construct_conll_token)
+   
+    #        if h_tree:
+    #            result_file.write(tree_to_conll_str(h_tree))
+    #            result_file.write('\n\n')
+    #        else:
+    #            failures += 1
+    #            forms = [token.form() for token in tree.full_token_yield()]
+    #            poss = [token.pos() for token in tree.full_token_yield()]
+    #            result_file.write(tree_to_conll_str(fall_back_left_branching(forms, poss)))
+    #            result_file.write('\n\n')
+    
+    #print "parse failures", failures
+    #print "parse time", total_time
+    
+    #print "eval.pl", "no punctuation"
+    #p = subprocess.Popen(["perl", "../util/eval.pl", "-g", test, "-s", result, "-q"])
+    #p.communicate()
+    
+    #print "eval.pl", "punctation"
+    #p = subprocess.Popen(
+    #    ["perl", "../util/eval.pl", "-g", test, "-s", result, "-q", "-p"])
+    #p.communicate()
+
+    #print "total time: ", total_time
+    # The following code is to count the number of derivations for a hypergraph (tree parser required)
+    #tree_parser.preprocess_grammar(grammar)
+
+    #trees = parse_conll_corpus(train , False, train_limit)
+    #if ignore_punctuation:
+    #    trees = disconnect_punctuation(trees)
+
+    #derCount = 0
+    #derMax = 0
+    #for tree in trees:
+    #    parser = tree_parser(grammar, tree)  # if tree parser is used
+    #    der = parser.count_derivation_trees()
+    #    if der > derMax:
+    #        derMax = der
+    #    derCount += der
+
+    #print "average number of derivations: ", 1.0*derCount/train_limit
+    #print "maximal number of derivations: ", derMax
+    #file = open("grammar.txt", 'w')
+    #g_l.linearize(grammar, primary_labelling, term_labelling, file)
+    #file.close()
+    #tree = trees.next()
+    #print tree.id_yield()
+    #print tree.recursive_partitioning()
+    #siblings = tree.siblings('0')
+    #print siblings
+
+
+
+def trainAndEval(strategy, labelling1, labelling2, fanout, parser_type, ignore_punctuation=False):
+    file = open('results.txt', 'w')
+    
+    term_labelling = d_i.the_terminal_labeling_factory().get_strategy('pos')
+    recursive_partitioning = d_i.the_recursive_partitioning_factory().getPartitioning('fanout-' + str(fanout) + strategy)
+    primary_labelling = d_l.the_labeling_factory().create_simple_labeling_strategy(labelling1, labelling2)
+    
+    trees = parse_conll_corpus(train, False, train_limit)
+    if ignore_punctuation:
+        trees = disconnect_punctuation(trees)
+    (n_trees, grammar) = d_i.induce_grammar(trees, primary_labelling, term_labelling.token_label, recursive_partitioning, start)
+
 
     total_time = 0.0
 
@@ -97,18 +203,20 @@ def main(ignore_punctuation=False):
                 poss = [token.pos() for token in tree.full_token_yield()]
                 result_file.write(tree_to_conll_str(fall_back_left_branching(forms, poss)))
                 result_file.write('\n\n')
-    
+
     print "parse failures", failures
     print "parse time", total_time
     
     print "eval.pl", "no punctuation"
-    p = subprocess.Popen(["perl", "../util/eval.pl", "-g", test, "-s", result, "-q"])
-    p.communicate()
-    
+    #p = subprocess.Popen(["perl", "../util/eval.pl", "-g", test, "-s", result, "-q"])
+    #p.communicate()
+    out = subprocess.check_output(["perl", "../util/eval.pl", "-g", test, "-s", result, "-q"])
+
     print "eval.pl", "punctation"
     p = subprocess.Popen(
         ["perl", "../util/eval.pl", "-g", test, "-s", result, "-q", "-p"])
-    p.communicate()
+    (out, err) = p.communicate()
+    print out
 
     print "total time: ", total_time
     # The following code is to count the number of derivations for a hypergraph (tree parser required)
@@ -129,14 +237,13 @@ def main(ignore_punctuation=False):
 
     print "average number of derivations: ", 1.0*derCount/train_limit
     print "maximal number of derivations: ", derMax
-    #file = open("grammar.txt", 'w')
-    #g_l.linearize(grammar, primary_labelling, term_labelling, file)
-    #file.close()
-    #tree = trees.next()
-    #print tree.id_yield()
-    #print tree.recursive_partitioning()
-    #siblings = tree.siblings('0')
-    #print siblings
+
+
+    file.close()
+
+
+
+
 
 if __name__ == '__main__':
     main()
