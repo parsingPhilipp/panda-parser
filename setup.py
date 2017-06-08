bbba14777e5630c21e9fcf96c5ff4369acfc3693
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2017-06-08 16:25:18 +0200

    c back-end update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ sterm_parser_repo = "git@gitlab.tcs.inf.tu-dresden.de:kilian/sterm-parser.git"
 dep_name = "sterm-parser"
 cython_dependency_src_path = path.join(here, "build", dep_name)
 the_branch = 'origin/MT_Hypergraph'
-the_commit = '138b7587714a557ae74b36d18bf46fc0d722b534'
+the_commit = '7ac378563841156486cb510da937e84f344b0e9d'
 sterm_include = [cython_dependency_src_path]
 # change if eigen is installed in the user-local directory
 # $COMPUTE_ROOT/usr/include/eigen3,
