515a3327b3a138585281a9d29f3d4ed4d3957988
teichm <markus.teichmann@mailbox.tu-dresden.de>
2017-06-16 15:00:41 +0200

    fix: local commit id changed to public

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ sterm_parser_repo = "git@gitlab.tcs.inf.tu-dresden.de:kilian/sterm-parser.git"
 dep_name = "sterm-parser"
 cython_dependency_src_path = path.join(here, "build", dep_name)
 the_branch = 'origin/MT_Hypergraph'
-the_commit = '8f61d1d76de8742c7038df74842cfc267489e7b4'
+the_commit = 'f043d0c8ee148aa16bc69e950e132ec8fc45bfdc'
 sterm_include = [cython_dependency_src_path]
 # change if eigen is installed in the user-local directory
 # $COMPUTE_ROOT/usr/include/eigen3,
