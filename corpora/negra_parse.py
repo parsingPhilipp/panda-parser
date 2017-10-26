25cfb57d6b818de477945fac31571fc78e8d2ff8
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2017-10-26 16:48:49 +0200

    updated negra parser to handle extended negra produced by disco-odp

--- corpora/negra_parse.py
+++ corpora/negra_parse.py
@@ -1,8 +1,9 @@
 # Parsing of the Negra corpus and capture of hybrid trees.
-
+from __future__ import print_function, unicode_literals
 from os.path import expanduser
 from hybridtree.constituent_tree import ConstituentTree
 from grammar.lcfrs import *
+from __builtin__ import str as text
 
 # Location of Negra corpus.
 negra_dir = 'res/negra-corpus/downloadv2'
@@ -24,19 +25,30 @@ def num_to_name(num):
 # file_name: string
 # return: list of hybrid trees obtained
 def sentence_names_to_hybridtrees(names, file_name):
-    negra = codecs.open(expanduser(file_name), encoding='iso-8859-1')
+    negra = codecs.open(expanduser(file_name), encoding='utf-8') #encoding='iso-8859-1')
     trees = []
     tree = None
     name = ''
     n_leaves = 0
     node_to_children = {}
+    mode = "STANDARD"
     for line in negra:
-        match_sent_start = re.search(r'^#BOS ([0-9]+)', line)
-        match_sent_end = re.search(r'^#EOS ([0-9]+)', line)
-        match_nont = \
-            re.search(r'^#([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t\n]+)$', line)
-        match_term = \
-            re.search(r'^([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t\n]+)$', line)
+        match_mode = re.search(r'^%%\s+word\s+lemma\s+tag\s+morph\s+edge\s+parent\s+secedge$', line)
+        if match_mode:
+            mode = "DISCO-DOP"
+            continue
+        match_sent_start = re.search(r'^#BOS\s+([0-9]+)', line)
+        match_sent_end = re.search(r'^#EOS\s+([0-9]+)$', line)
+        if mode == "STANDARD":
+            match_nont = \
+                re.search(r'^#([0-9]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\n)?$', line)
+            match_term = \
+                re.search(r'^([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\n)?$', line)
+        elif mode == "DISCO-DOP":
+            match_nont = \
+                re.search(r'^#([0-9]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\s+([^\s]+))?(\n)?$', line)
+            match_term = \
+                re.search(r'^([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\s+([^\s]+))?(\n)?$', line)
         if match_sent_start:
             this_name = match_sent_start.group(1)
             if this_name in names:
@@ -53,9 +65,14 @@ def sentence_names_to_hybridtrees(names, file_name):
         elif tree:
             if match_nont:
                 id = match_nont.group(1)
-                nont = match_nont.group(2)
-                edge = match_nont.group(4)
-                parent = match_nont.group(5)
+                if mode == "STANDARD":
+                    nont = match_nont.group(2)
+                    edge = match_nont.group(4)
+                    parent = match_nont.group(5)
+                elif mode == "DISCO-DOP":
+                    nont = match_nont.group(3)
+                    edge = match_nont.group(5)
+                    parent = match_nont.group(6)
                 tree.set_label(id, nont)
                 tree.node_token(id).set_edge_label(edge)
                 if parent == '0':
@@ -64,9 +81,14 @@ def sentence_names_to_hybridtrees(names, file_name):
                     tree.add_child(parent, id)
             elif match_term:
                 word = match_term.group(1)
-                pos = match_term.group(2)
-                edge = match_nont.group(4)
-                parent = match_term.group(5)
+                if mode == "STANDARD":
+                    pos = match_term.group(2)
+                    edge = match_term.group(4)
+                    parent = match_term.group(5)
+                elif mode == "DISCO-DOP":
+                    pos = match_term.group(3)
+                    edge = match_term.group(5)
+                    parent = match_term.group(6)
                 n_leaves += 1
                 leaf_id = str(100 + n_leaves)
                 if parent == '0':
@@ -118,26 +140,29 @@ def hybridtree_to_sentence_name(tree, idNum):
 
     for leaf in tree.full_yield():
         token = tree.node_token(leaf)
-        morph = '--'
-        line = [str(token.form()), token.pos(), morph, token.edge()]
+        morph = u'--'
+        # if not isinstance(token.form(), str):
+        #     print(token.form(), type(token.form()))
+        #     assert isinstance(token.form(), str)
+        line = [token.form(), token.pos(), morph, token.edge()]
 
         if leaf in tree.id_yield() and leaf not in tree.root:
             if tree.parent(leaf) is None or tree.parent(leaf) not in idNum:
-                print tree, leaf, tree.full_yield(), map(str,tree.full_token_yield()), tree.parent(leaf), tree.parent(leaf) in idNum
-            lines.append('\t'.join(line + [str(idNum[tree.parent(leaf)])]) + '\n')
+                print(tree, leaf, tree.full_yield(), map(text, tree.full_token_yield()), tree.parent(leaf), tree.parent(leaf) in idNum)
+            lines.append(u'\t'.join(line + [text(idNum[tree.parent(leaf)])]) + u'\n')
         else:
-            lines.append('\t'.join(line + ['0']) + '\n')
+            lines.append(u'\t'.join(line + [u'0']) + u'\n')
 
     for id in tree.ids():
         token = tree.node_token(id)
-        morph = '--'
+        morph = u'--'
 
-        line = ['#' + str(idNum[id]), str(token.category()), morph, token.edge()]
+        line = [u'#' + text(idNum[id]), text(token.category()), morph, token.edge()]
 
         if id in tree.root:
-            lines.append('\t'.join(line + ['0']) + '\n')
+            lines.append(u'\t'.join(line + [u'0']) + u'\n')
         elif id not in tree.root:
-            lines.append('\t'.join(line + [str(idNum[tree.parent(id)])]) + '\n')
+            lines.append(u'\t'.join(line + [text(idNum[tree.parent(id)])]) + u'\n')
 
     return lines
 
@@ -157,9 +182,9 @@ def hybridtrees_to_sentence_names(trees, counter, length):
             idNum = dict()
             for root in tree.root:
                 generate_ids_for_inner_nodes(tree, root, idNum)
-            sentence_names.append('#BOS ' + str(counter) + '\n')
+            sentence_names.append(u'#BOS ' + text(counter) + u'\n')
             sentence_names.extend(hybridtree_to_sentence_name(tree, idNum))
-            sentence_names.append('#EOS ' + str(counter) + '\n')
+            sentence_names.append(u'#EOS ' + text(counter) + u'\n')
             counter += 1
 
     return sentence_names
