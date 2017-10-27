545ab60087a822e4cdd67192d0efba113dd41879
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2017-10-27 16:20:45 +0200

    setup.py: updated Split-Merge backend

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ sterm_parser_repo = "git@gitlab.tcs.inf.tu-dresden.de:kilian/sterm-parser.git"
 dep_name = "sterm-parser"
 cython_dependency_src_path = path.join(here, "build", dep_name)
 the_branch = 'origin/MT_Hypergraph'
-the_commit = '3a4eb0a1b4cc6f276cd2f7e37b180a76d2739512'
+the_commit = '484dcfbc0df2b2d3a75609f0df653c4d866663a5'
 sterm_include = [cython_dependency_src_path]
 # change if eigen is installed in the user-local directory
 # $COMPUTE_ROOT/usr/include/eigen3,
