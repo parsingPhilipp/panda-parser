706af15412d62f3606c0645af2de6cecd009a3d2
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2017-11-28 16:13:44 +0100

    updated setup.py to newer version of STermParser

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ sterm_parser_repo = "git@gitlab.tcs.inf.tu-dresden.de:kilian/sterm-parser.git"
 dep_name = "sterm-parser"
 cython_dependency_src_path = path.join(here, "build", dep_name)
 the_branch = 'origin/MT_Genetic'
-the_commit = '1c89dadda0d346a41eb4e6ed46d801be318fdba3'
+the_commit = 'e888c0ac0dc8133d8cb4b6eebc1b5c845346a31e'
 sterm_include = [cython_dependency_src_path]
 # change if eigen is installed in the user-local directory
 # $COMPUTE_ROOT/usr/include/eigen3,
